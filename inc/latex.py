4d84b3c60071834d9477eab248ebcfb5e8e90305
eons <eons@eons.dev>
2022-01-13 22:23:12 -0800

    updates self to this

--- inc/latex.py
+++ inc/latex.py
@@ -5,20 +5,20 @@ from distutils.dir_util import copy_tree, mkpath
 from ebbs import Builder
 
 class latex(Builder):
-    def __init__(self, name="LaTeX document builder"):
+    def __init__(this, name="LaTeX document builder"):
         super().__init__(name)
     
-        self.supportedProjectTypes.append("doc")
+        this.supportedProjectTypes.append("doc")
 
     #Required Builder method. See that class for details.
-    def Build(self):
-        if os.path.exists(self.buildPath):
-            logging.info(f"DELETING {self.buildPath}")
-            shutil.rmtree(self.buildPath)
+    def Build(this):
+        if os.path.exists(this.buildPath):
+            logging.info(f"DELETING {this.buildPath}")
+            shutil.rmtree(this.buildPath)
 
-        logging.info(f"Using build path {self.buildPath}")
-        mkpath(self.buildPath)
-        copy_tree(self.srcPath, os.path.join(self.buildPath))
-        os.chdir(self.buildPath)
-        self.RunCommand("latex -interaction=nonstopmode main.tex")
-        self.RunCommand("pdflatex -interaction=nonstopmode main.tex")
+        logging.info(f"Using build path {this.buildPath}")
+        mkpath(this.buildPath)
+        copy_tree(this.srcPath, os.path.join(this.buildPath))
+        os.chdir(this.buildPath)
+        this.RunCommand("latex -interaction=nonstopmode main.tex")
+        this.RunCommand("pdflatex -interaction=nonstopmode main.tex")
